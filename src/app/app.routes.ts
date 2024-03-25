5baa06faa50066dc76202625e53f325949664857
Tounzitoun <fael.@outlook.fr>
2024-03-25 15:37:08 +0100

    Mise en place du front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import {loginUserComponent} from "./login-user/login-user.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [path:'list', component: loginUserComponent];
